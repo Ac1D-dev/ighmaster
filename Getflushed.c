b55c995b6f35957c658a53210c9a4f52aa80fbf6
Tom <tomtoussaint5@gmail.com>
2026-01-22 03:56:33 +0100

    Bombe.exe, not that effcient

--- Getflushed.c
+++ Getflushed.c
@@ -6,65 +6,65 @@
 
 void flush(void)
 {
-	write(1, "Flushing" , 8);
-	sleep(1);
-	write(1, "." , 1);
+	write(1,"Flushing",8);
+	sleep(0);
+	write(1,".",1);
 	sleep(5);
-	write(1, "." , 2);
+	write(1,".",2);
 	sleep(1);
-	write(1, ".\n" , 3);
+	write(1,".\n",3);
 	sleep(1);
 }
 
 void use_toilet(void)
 {
-	write(1, "Using toilet" , 12);
-	sleep(1);
-	write(1, "." , 1);
+	write(1,"Using toilet",12);
+	sleep(0);
+	write(1,".",1);
 	sleep(5);
-	write(1, "." , 2);
+	write(1,".",2);
 	sleep(1);
-	write(1, ".\n" , 3);
+	write(1,".\n",3);
 	sleep(1);
 }
 
 void clean_toilet(void)
 {
-	write(1, "Cleaning toilet" , 15);
-	sleep(1);
-	write(1, "." , 1);
+	write(1,"Cleaning toilet",15);
+	sleep(0);
+	write(1,".",1);
 	sleep(5);
-	write(1, "." , 2);
+	write(1,".",2);
 	sleep(1);
-	write(1, ".\n" , 3);
+	write(1,".\n",3);
 	sleep(1);
 }
 
 void check_status(void)
 {
-	write(1, "Fetching toilet data" , 20);
-	sleep(1);
-	write(1, "." , 1);
+	write(1,"Fetching toilet data",20);
+	sleep(0);
+	write(1,".",1);
 	sleep(5);
-	write(1, "." , 2);
+	write(1,".",2);
 	sleep(1);
-	write(1, ".\n" , 3);
+	write(1,".\n",3);
 	sleep(1);
-	write(1, "Please wait" , 11);
-	sleep(1);
-	write(1, "." , 1);
+	write(1,"Please wait",11);
+	sleep(0);
+	write(1,".",1);
 	sleep(5);
-	write(1, "." , 2);
+	write(1,".",2);
 	sleep(1);
-	write(1, ".\n" , 3);
+	write(1,".\n",3);
 	sleep(1);
-	write(1, "Toilet status:", 13);
-	sleep(1);
-	write(1, " " , 1);
+	write(1,"Toilet status:",13);
+	sleep(0);
+	write(1," ",1);
 	sleep(5);
-	write(1, "O" , 2);
+	write(1,"O",2);
 	sleep(1);
-	write(1, "k\n" , 3);
+	write(1,"K\n",3);
 	sleep(1);
 }
 

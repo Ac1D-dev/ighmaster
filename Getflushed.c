dff5e2de0c62f13f3f4ecd8e5c6c6e3a51d00fbb
Unu <156459970+UnuMwetwetwe@users.noreply.github.com>
2026-01-22 03:12:42 +0100

    now accurately tracks user data

--- Getflushed.c
+++ Getflushed.c
@@ -1,25 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 /* Toilet utility functions */
 
 void flush(void)
 {
 	printf("Flushing...\n");
+	sleep(5);
 }
 
 void use_toilet(void)
 {
 	printf("Using toilet...\n");
+	sleep(5);
 }
 
 void clean_toilet(void)
 {
 	printf("Cleaning toilet...\n");
+	sleep(5);
 }
 
 void check_status(void)
 {
+	printf("Fetching toilet data...\n");
+	sleep(5);
+	printf("Please wait...\n");
+	sleep(5);
 	printf("Toilet status: OK\n");
 }
 
